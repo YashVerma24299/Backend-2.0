8ccba525817ff333719ef57938fecf8412acc59c
Yash <ritikverma23@gmail.com>
2026-02-11 23:59:10 +0530

    work on ES Module

---
+++ Node/7_Es_Module/index.js
@@ -0,0 +1,40 @@
+import { log } from "console";
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const todos = [];
+
+const showMenu = () => {
+  console.log("\n1. Add a Task ");
+  console.log("2. View Tasks");
+  console.log("3. Exit");
+  rl.question("Choose a option: ", handleInput);
+};
+
+const handleInput = (option) => {
+  if (option == 1) {
+    rl.question("Enter the Task: ", (task) => {
+      todos.push(task);
+      console.log("Task Added: ", task);
+      showMenu();
+    });
+  } else if (option == 2) {
+    console.log("\n Your Todo List");
+    todos.map((task,ind)=>{
+        console.log(`${ind+1}. ${task}`);
+        showMenu();
+    })
+  }
+  else if(option==3){
+    console.log("Good Bye");
+    rl.close();
+  }
+  else{
+    console.log("Invalid Option");
+    showMenu();
+  }
+};
